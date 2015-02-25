3e78a2765ce208a74145f12a058ac671f34ebd36
Ocs1 <a@b.com>
2015-02-25 11:39:54 +0330

    Initial commit

---
+++ src/main/java/com/concretepage/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.concretepage.config;  
+  
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+  
+@Configuration 
+@ComponentScan("com.concretepage") 
+@EnableWebMvc
+@Import(HibernateConfiguration.class)
+public class AppConfig {  
+
+}  
